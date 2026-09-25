ccdc03f4b6eb314d32b79b663bd061eaa4924d87
Miraj Hackz <dracomirajofficial@gmail.com>
2026-09-25 11:37:36 +0530

    Update wedding date in countdown script

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ scratchBtn.addEventListener("click", () => {
 // =======================
 
 // Change your wedding date here
-const weddingDate = new Date("March 12, 2027 08:00:00").getTime();
+const weddingDate = new Date("March 13, 2027 08:00:00").getTime();
 
 function countdown(){
 
